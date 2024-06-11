673ef553e8309ef8ce93a55b20c728098b1c6337
popododo0720 <gimsu@gimsuui-iMac.local>
2024-06-11 17:51:24 +0900

    first commit

---
+++ src/main/java/com/toyproj/ToyprojApplication.java
@@ -0,0 +1,13 @@
+package com.toyproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyprojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toyproj/ToyprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyprojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
